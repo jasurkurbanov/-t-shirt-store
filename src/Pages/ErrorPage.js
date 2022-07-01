3b75f4c4cd60abfdaf64f621b2e531a2cb0f1c4c
Mirjalol Urakov <mirgalol19@gmail.com>
2022-07-01 17:57:00 +0500

    feat: banner page

--- src/Pages/ErrorPage.js
+++ src/Pages/ErrorPage.js
@@ -3,7 +3,7 @@ import React from "react";
 function ErrorPage() {
   return (
     <div>
-      <h1>EROR! 404</h1>
+      <h1>ERROR! 404</h1>
     </div>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,35 @@
 import React from "react";
+import bannerimg1 from "../assets/images/home/home-banner/banner-img-1.png";
+import bannerimg2 from "../assets/images/home/home-banner/banner-img-2.png";
+import bannerimg3 from "../assets/images/home/home-banner/banner-img-3.png";
 
 function Home() {
   return (
-    <div>
-      <h1>THIS IS THE HOME PAGE</h1>
+    <div className="banner ">
+      <div className="banner__content wrapper ">
+        <div className="item__left">
+          <div className="banner__title">
+            <h1>Get the Latest Dress</h1>
+            <h1>Models From Us</h1>
+          </div>
+          <div className="banner__subtitle">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>{" "}
+            <p>In eget gravida leo, nec iaculis diam.Nam bibendum mi</p>
+            <p>sed sem finibus ullamcorper.</p>
+          </div>
+          <button className="button__item">Shop Now</button>
+        </div>
+        <div className="item__right">
+          <div className="img__block1">
+            <h1>More list</h1>
+            <img className="b__img1" src={bannerimg1} alt="" />
+            <h2>Suit</h2>
+            <img className="b__img2" src={bannerimg2} alt="" />
+            <h3>Shirt</h3>
+          </div>
+          <img className="b__img3" src={bannerimg3} alt="" />
+        </div>
+      </div>
     </div>
   );
 }
