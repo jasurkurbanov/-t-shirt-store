cc1fd3d3193e2db155115570280e84ae62c6e51a
Umar <umaribragimovich555@gmail.com>
2022-07-02 08:41:51 +0500

    feat : header component-v1

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,42 @@
 import React from "react";
+import Logo from "../assets/images/shared/logo.svg";
+import { FaRegHeart } from "react-icons/fa";
+import { BsHandbag } from "react-icons/bs";
+import { HiOutlineUser } from "react-icons/hi";
+import { AiOutlineSearch } from "react-icons/ai";
 
 function Home() {
   return (
-    <div>
-      <h1 className="first">THIS IS THE HOME PAGE</h1>
+    <div className="header">
+      <div className="header_content">
+        <div className="logo">
+          <img src={Logo} />
+
+          <p className="logo_text">mangcoding Store</p>
+        </div>
+        <div className="navbar">
+          <ul>
+            <li>Home</li>
+            <li>Shop</li>
+            <li>About us</li>
+            <li>Contact</li>
+            <li>
+              <BsHandbag />
+            </li>
+            <li>
+              <FaRegHeart />
+            </li>
+            <li>
+              <HiOutlineUser />
+            </li>
+          </ul>
+
+          <div className="search_bar">
+            <input type="text" placeholder="Search" name="search" />
+            <AiOutlineSearch />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
